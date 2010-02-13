aced596e8ef66aaa8b5412d6db4083d53a97a2de
Nathan Hamblen <nathan@technically.us>
2010-02-13 15:03:06 -0500

    knockoff for markdown transform, preview and publish actions

--- project/build/PosterousProject.scala
+++ project/build/PosterousProject.scala
@@ -3,4 +3,6 @@ import sbt._
 class PosterousProject(info: ProjectInfo) extends PluginProject(info) {
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   val dispatch = "net.databinder" %% "dispatch-http" % "0.7.0-beta2"
+  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
+  val knockoff = "com.tristanhunt" %% "knockoff" % "0.6.1-8"
 }
--- src/main/scala/PosterousPost.scala
+++ src/main/scala/PosterousPost.scala
@@ -3,8 +3,10 @@ import sbt._
 
 import dispatch._
 import java.net.URI
+import com.tristanhunt.knockoff.DefaultDiscounter._
+import scala.xml.Node
 
-trait Post extends BasicDependencyProject {
+trait Post extends BasicDependencyProject with FileTasks {
   def posterousCredentials = Path.userHome / ".posterous"
   private lazy val posterousProperties = {
     val props = new java.util.Properties
@@ -19,7 +21,17 @@ trait Post extends BasicDependencyProject {
   /** Strings to tag the post with, defaults to the project name, organization, and Scala build versions */
   def postTags = name :: organization :: crossScalaVersions.map { "Scala " + _ }.toList
   def postTitle = "%s %s".format(name, version)
-  def postBody = "release notes <i>stuff</i>"
+  def notesPath = path("notes")
+  override def watchPaths = super.watchPaths +++ (notesPath * "*.txt")
+  def versionNotesPath = notesPath / (version + ".txt")
+  def aboutNotesPath = notesPath / "about.txt"
+  def postBodyTxts = versionNotesPath :: aboutNotesPath :: Nil
+  def txtToXml(txt: Path) =
+    if (txt.exists)
+      toXML(knockoff(scala.io.Source.fromFile(txt.asFile).mkString))
+    else
+      Nil
+  def postBody = postBodyTxts flatMap txtToXml
   def postSource = "<a href='http://github.com/n8han/posterous-sbt'>posterous-sbt plugin</a>"
 
   lazy val publishNotes = publishNotesAction
@@ -29,7 +41,7 @@ trait Post extends BasicDependencyProject {
         "site_id" -> postSiteId,
         "title" -> postTitle,
         "tags" -> postTags.map { _.replace(",","_") }.removeDuplicates.mkString(","),
-        "body" -> postBody,
+        "body" -> postBody.mkString,
         "source" -> postSource
       )
     (new Http)(post <> { rsp =>
@@ -39,6 +51,18 @@ trait Post extends BasicDependencyProject {
       }
     })
     None
+  }
+  def notesOutputPath = outputPath / ("%s-notes.html" format artifactBaseName)
+  lazy val previewNotes = previewNotesAction
+  def previewNotesAction = task {
+    FileUtilities.write(notesOutputPath.asFile, log) { writer =>
+      writer.write(<html> { postBody } </html> toString)
+      None
+    } orElse {
+      log.success("Saved release notes: " + notesOutputPath)
+      tryBrowse(notesOutputPath.asFile.toURI)
+      None
+    }
   }
   def tryBrowse(uri: URI) { 
     try {
