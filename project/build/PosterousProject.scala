f748ae09d7dc1f5813d8ed53ceff7fa0f8ad67ea
Nathan Hamblen <nathan@technically.us>
2010-02-14 18:05:25 -0500

    snapshots, use 0.1.0 of self

--- project/build/PosterousProject.scala
+++ project/build/PosterousProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class PosterousProject(info: ProjectInfo) extends PluginProject(info) {
+class PosterousProject(info: ProjectInfo) extends PluginProject(info) with posterous.Publish {
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   val dispatch = "net.databinder" %% "dispatch-http" % "0.7.0-beta2"
 
