0f48668c006d42c0ae57cf9b9b632bce4c4be859
Nathan Hamblen <nathan@technically.us>
2010-02-15 12:08:11 -0500

    extraTags for extra cool projects

--- project/build/PosterousProject.scala
+++ project/build/PosterousProject.scala
@@ -6,6 +6,8 @@ class PosterousProject(info: ProjectInfo) extends PluginProject(info) with poste
 
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
   val knockoff = "com.tristanhunt" %% "knockoff" % "0.6.1-8"
+  
+  override def extraTags = "knockoff" :: "dispatch" :: "sbt" :: super.extraTags
 
   override def managedStyle = ManagedStyle.Maven
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
