c2d45d4b2e0a82ea18d76167044dd37efc3801b0
Nathan Hamblen <nathan@technically.us>
2010-02-15 12:53:09 -0500

    0.1.1

--- project/build/PosterousProject.scala
+++ project/build/PosterousProject.scala
@@ -1,8 +1,7 @@
 import sbt._
 
 class PosterousProject(info: ProjectInfo) extends PluginProject(info) with posterous.Publish {
-  val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-  val dispatch = "net.databinder" %% "dispatch-http" % "0.7.0-beta2"
+  val dispatch = "net.databinder" %% "dispatch-http" % "0.7.0"
 
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
   val knockoff = "com.tristanhunt" %% "knockoff" % "0.6.1-8"
