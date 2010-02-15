8b5ae3c840c484eadb59a1fef866c78043a97345
Nathan Hamblen <nathan@technically.us>
2010-02-15 12:10:32 -0500

    repos

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,7 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-	val posterous = "net.databinder" % "posterous-sbt" % "0.1.0"
+  val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
+  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.0"
 }
