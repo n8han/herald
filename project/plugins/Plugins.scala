73ee820bcda9fe8c42c7a01675500d75383615b3
Nathan Hamblen <nathan@technically.us>
2010-02-15 13:08:56 -0500

    version bumps

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-  val posterous = "net.databinder" % "posterous-sbt" % "0.1.0"
+  val posterous = "net.databinder" % "posterous-sbt" % "0.1.1"
 }
